5674751651b7c42eecda72ec4ca2aa5fcc3fdc14
Kosuke Aoki <mail@noraworld.com>
2025-07-05 17:25:13 +0900

    Add option to insert text before and after response

--- app.py
+++ app.py
@@ -24,7 +24,17 @@ if dirpath:
     os.makedirs(dirpath, exist_ok=True)
 
 with open(os.getenv('OUTPUT_FILE'), 'w', encoding='utf-8') as f:
+    content = ''
+
+    if os.getenv('TEXT_BEFORE_RESULT') != '' and os.getenv('TEXT_BEFORE_RESULT') is not None:
+        content += os.getenv('TEXT_BEFORE_RESULT') + os.linesep
+
+    content += completion.choices[0].message.content
+
+    if os.getenv('TEXT_AFTER_RESULT') != '' and os.getenv('TEXT_AFTER_RESULT') is not None:
+        content = content.rstrip(os.linesep) + os.linesep + os.linesep + os.getenv('TEXT_AFTER_RESULT')
+
     if os.getenv('TRAILING_NEWLINE') == 'true':
-        f.write(completion.choices[0].message.content.rstrip(os.linesep) + os.linesep)
-    else:
-        f.write(completion.choices[0].message.content)
+        content = content.rstrip(os.linesep) + os.linesep
+
+    f.write(content)
